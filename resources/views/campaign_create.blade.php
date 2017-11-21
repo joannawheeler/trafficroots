5c10cbc4f63114d8d59d99a7c78399175d562bbf
Cary White <admin@trafficroots.com>
2017-11-21 09:29:15 +0000

    tweak

--- resources/views/campaign_create.blade.php
+++ resources/views/campaign_create.blade.php
@@ -91,18 +91,24 @@
                        <div class="form-group">
                             <label for="submit" class="col-md-4 control-label">Submit Campaign</label>
                             <div class="col-md-6">
-                                <input type="submit" class="btn btn-primary" name="submit" id="submit" />
+                                <input type="submit" class="btn btn-primary" name="submit" id="submit" /></form>&nbsp;&nbsp;<a href="/campaigns"><button type="button" class="btn btn-white">Cancel</button></a> 
                             </div>
                         </div>
-                </form>
-                    <div style="background: url('img/OakTree.jpg') 50% 0 no-repeat;">
+                
+                    <div>
                         <h3>Campaign:</h3>
-                        [kam <b>peyn</b>]
+                        [kam <b>peyn</b>] - 
                         <i>noun</i>
                         <ul><li>a systematic course of aggressive activities for some specific purpose</li></ul>
                         
                         <div class="well">
-                            A Trafficroots Campaign is a targeted advertising plan that consists of at least one Creative.  Campaigns target a specific Location Type and all Creatives must conform to the specified Category. <br />On this page you are creating a new Campaign by naming it and selecting a Campaign Type, Location Type and Category.  We offer Cost Per Click (CPC) and Cost Per Milli (CPM) Campaign Types. Once created, additional Campaign targeting options such as City or State, as well as your Bid amount are set on the Campaign Edit page.
+                            <ul>
+                                <li>A Trafficroots Campaign is a targeted advertising plan that consists of at least one Creative.</li>
+                                <li>Campaigns target a specific Location Type and all Creatives and their destination Links must conform to the specified Category.</li>
+                                <li>On this page you are creating a new Campaign by naming it and selecting a Campaign Type, Location Type and Category.</li>
+                                <li>We offer Cost Per Click (CPC) and Cost Per Milli (CPM) Campaign Types. </li>
+                                <li>Once created, additional Campaign targeting options such as City or State, as well as your Bid amount are set on the Campaign Edit page.</li>
+                             </ul>
                         </div>
                     </div>
                 </div>
