c6890ee817d6a08cd4051a7dd4f8a1f9030a707a
Cary White <cary@sendlane.com>
2017-11-21 00:45:18 -0800

    help screen

--- resources/views/campaign_create.blade.php
+++ resources/views/campaign_create.blade.php
@@ -91,10 +91,20 @@
                        <div class="form-group">
                             <label for="submit" class="col-md-4 control-label">Submit Campaign</label>
                             <div class="col-md-6">
-                                <input type="submit" name="submit" id="submit" />
+                                <input type="submit" class="btn btn-primary" name="submit" id="submit" />
                             </div>
                         </div>
                 </form>
+                    <div style="background: url('img/OakTree.jpg') 50% 0 no-repeat;">
+                        <h3>Campaign:</h3>
+                        [kam <b>peyn</b>]
+                        <i>noun</i>
+                        <ul><li>a systematic course of aggressive activities for some specific purpose</li></ul>
+                        
+                        <div class="well">
+                            A Trafficroots Campaign is a targeted advertising plan that consists of at least one Creative.  Campaigns target a specific Location Type and all Creatives must conform to the specified Category. <br />On this page you are creating a new Campaign by naming it and selecting a Campaign Type, Location Type and Category.  We offer Cost Per Click (CPC) and Cost Per Milli (CPM) Campaign Types. Once created, additional Campaign targeting options such as City or State, as well as your Bid amount are set on the Campaign Edit page.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
