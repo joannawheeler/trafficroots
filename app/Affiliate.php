c424851ebd5173721b3f7d239075b871f0fece9a
Cary White <cary@sendlane.com>
2018-02-13 23:56:04 -0800

    missing models

--- app/Affiliate.php
+++ app/Affiliate.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Affiliate extends Model
 {
-	protected $fillable('name', 'url', 'added_by', 'created_at', 'updated_at');
+	protected $fillable = array('name', 'url', 'added_by', 'created_at', 'updated_at');
 	protected $table = 'affiliates';
 }
