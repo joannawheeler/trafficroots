f9406e1cb4f28fbd839a337d6280695c0321e0b1
Jackie Campos <Jackie@Christians-MacBook-Pro-2.local>
2018-06-21 02:54:29 -0700

    Edit Custom Ads functionality. Please note there are few bugs that need fixing. THIS IS NOT COMPLETE.

--- app/AdCreative.php
+++ app/AdCreative.php
@@ -8,4 +8,14 @@ class AdCreative extends Model
 {
 	protected $table = 'ad_creatives';
 	protected $fillable = ['ad_id','weight','status','media_id','link_id','folder_id','created_at','updated_at'];
+	
+	public function medias()
+    {
+        return $this->belongsTo('App\Media', 'media_id');
+    }
+    
+    public function links()
+    {
+        return $this->belongsTo('App\Links', 'link_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,15 @@ Route::get('/custom_ad/{handle}', 'ZoneController@getCustomAd');
 Route::post('/custom_ad', 'ZoneController@postCustomAd');
 Route::get('/pause_custom_ad/{id}', 'ZoneController@pauseCustomAd');
 Route::get('/resume_custom_ad/{id}', 'ZoneController@resumeCustomAd');
+Route::patch('/zones/{zone}', 'ZoneController@edit');
+Route::get('/edit_custom_ad/{id}', 'ZoneController@editCustomAd');
+Route::post('/update_frequencyAd', 'ZoneController@updateFrequencyCap');
+Route::post('/update_impressionCap', 'ZoneController@updateImpressionCap');
+Route::post('/update_weight', 'ZoneController@updateWeight');
+Route::post('/update_start', 'ZoneController@updateStartDate');
+Route::post('/update_end', 'ZoneController@updateEndDate');
+Route::get('/custom_creatives/{id}', 'ZoneController@createCreative');
+
 
 Auth::routes();
 Route::get('/send_confirmation', 'HomeController@sendConfirmation');
