edf30ed58c3ae69b9dde699a5d9ceaf05eb61dd9
Joanna Wheeler <joannachristinewheeler@gmail.com>
2018-08-02 12:09:28 -0700

    styled publisher support page for web screen size

--- resources/views/publisherSupport.blade.php
+++ resources/views/publisherSupport.blade.php
@@ -1,35 +1,8 @@
 @extends('layouts.landing')
 @section('title', 'Support')
 @section('css')
-<!-- <style>
-
-</style> -->
-@endsection
-<!-- @section('js')
----EXAMPLE
-  <script src="{{ URL::asset('js/plugins/dataTables/datatables.min.js') }}"></script>
----END EXAMPLE
-@endsectionß
-@section('content')
+<!-- @section('content')
  -->
-
-<!-- TODO: remove .html formatting and change format to php, uncomment php -->
-<!-- HTML formatting is for testing only. Once development environment is configured, will convert to php format -->
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Advertiser Support</title>
-    <meta charset="ut-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="support.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </head>
-  <body> -->
-    <!-- <p> $output </p> -->
-
 @section('content')
 <style>
   ul {
@@ -38,8 +11,12 @@
     list-style-type: none;
   }
 
+  li {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+
   a {
-    /*color: #1ab394*/
     color: black
   }
 
@@ -53,7 +30,10 @@
   }
 
   .topicsSection {
-    padding: 50px 50px 120px 120px;
+    padding-left: 150px;
+    padding-right: 150px;
+    padding-bottom: 70px;
+    padding-top: 70px;
   }
 
   .switchToAdvText {
@@ -75,8 +55,31 @@
     padding: 0px
   }
 
-  .topicHeadAndList {
+  .topRow {
+    padding-bottom: 50px;
+  }
 
+  .temporaryNav {
+    background-color: rgba(176,196,222,.75);
+    height: 10%;
+    position: fixed;
+    width: 100%
+  }
+
+  .imgTitleSection {
+    background-image: url('https://www.renmoneyng.com/images/uploads/faqs.png');
+    background-size: cover;
+    margin-top: 5.25%
+  }
+
+  .textDocIcon {
+    height: 25px;
+    width: 25px;
+  }
+
+  .videoIcon {
+    height: 25px;
+    width: 40px;
   }
 
 
@@ -92,29 +95,32 @@
   }
 
 </style>
+
+
 <div class="container-fluid">
   <section>
-    <div class="row" style="background-color: rgba(176,196,222,.75); height: 8.5%; position: fixed; width: 100%">
+    <div class="row temporaryNav">
     </div>
   </section>
 
   <section>
-    <div class="row alignCenterVertical" style="background-image: url('https://www.renmoneyng.com/images/uploads/faqs.png'); background-size: cover; margin-top: 4.5%">
-      <div class="col-12-sm" style="">
-        <h1 class="title" style="font-weight: bolder; font-size: 60px; color: white; ">Publisher Support</h1>
-        <p class="text-center switchToAdvText"; ">Switch to<a id="switchToAdvLink" href="https://trafficroots.test/advertiserSupport"> Advertiser Support</a></p>
+    <div class="row imgTitleSection alignCenterVertical" style="">
+      <div class="col-12-sm">
+        <h1 style="font-weight: bolder; font-size: 60px; color: white;">Publisher Support</h1>
+        <p class="text-center switchToAdvText">Switch to<a id="switchToAdvLink" href="https://trafficroots.test/advertiserSupport"> Advertiser Support</a></p>
       </div>
     </div>
   </section>
 
-  <section>
-    <div class="row">
+  <section class="topicsSection">
+    <div class="row topRow">
       <div class="col-sm-4">
-        <h2 class="topics">Getting Started</h2>
+        <h2 class="topics"> Getting Started</h2>
         <ul>
-          <li><a href="#">Video: How to Get Started</a></li>
-          <li><a href="#">Am I a publisher or am I an advertiser?</a></li><li><a href="#">Tips for Success</a></li>
-          <li><a href="#">How long does it take for my campaign to be approved?
+          <li><img class="videoIcon" src="https://banner2.kisspng.com/20180405/tyq/kisspng-youtube-computer-icons-font-awesome-logo-clip-art-video-icon-5ac5bcbf5e7a50.846113781522908351387.jpg"><a href="#">&nbsp&nbspVideo: How to Get Started</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspAm I a publisher or am I an advertiser?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspTips for Success</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?
             </a>
           </li>
         </ul>
@@ -122,45 +128,45 @@
       <div class="col-sm-4">
         <h2 class="topics">Campaigns</h2>
       <ul>
-          <li><a href="#">Why was my campaign rejected?</a></li>
-          <li><a href="#">How long does it take for my campaign to be approved?</a></li>
-          <li><a href="#">Do I receive an Invoice?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspWhy was my campaign rejected?</a></li>
+          <li><img class="videoIcon" src="https://banner2.kisspng.com/20180405/tyq/kisspng-youtube-computer-icons-font-awesome-logo-clip-art-video-icon-5ac5bcbf5e7a50.846113781522908351387.jpg"><a href="#">&nbsp&nbspVideo: How long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspDo I receive an Invoice?</a></li>
         </ul>
       </div>
       <div class="col-sm-4">
         <h2 class="topics">Pricing</h2>
         <ul>
-          <li><a href="#">How much is it to advertise?</a></li>
-          <li><a href="#">Do I receive an Invoice?</a></li>
-          <li><a href="#">How do I add funds to my acount?</a></li>
-          <li><a href="#">What payment options do you accept?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow much is it to advertise?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspWhat payment options do you accept?</a></li>
         </ul>
       </div>
     </div>
     <div class="row">
       <div class="col-sm-4">
         <h2 class="topics">Payments</h2>
-        <ul>
-        <li><a href="#">Video: How to Get Started</a></li>
-        <li><a href="#">Tips for Success</a></li>
-          <li><a href="#">How much is it to advertise?</a></li>
-          <li><a href="#">Do I receive an Invoice?</a></li>
+        <ul id="Payments">
+          <li><img class="videoIcon" src="https://banner2.kisspng.com/20180405/tyq/kisspng-youtube-computer-icons-font-awesome-logo-clip-art-video-icon-5ac5bcbf5e7a50.846113781522908351387.jpg"><a href="#">&nbsp&nbspVideo: How to Get Started</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspTips for Success</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
         </ul>
       </div>
       <div class="col-sm-4">
         <h2 class="topics">Something Else</h2>
         <ul>
-          <li><a href="#">Why was my campaign rejected?</a></li>
-          <li><a href="#">How long does it take for my campaign to be approved?</a></li>
-          <li><a href="#">How long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspWhy was my campaign rejected?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
         </ul>
       </div>
       <div class="col-sm-4">
         <h2 class="topics">Something Else</h2>
         <ul>
-          <li><a href="#">How much is it to advertise?</a></li>
-          <li><a href="#">Do I receive an Invoice?</a></li>
-          <li><a href="#">How do I add funds to my acount?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow much is it to advertise?</a></li>
+          <li><img class="videoIcon" src="https://banner2.kisspng.com/20180405/tyq/kisspng-youtube-computer-icons-font-awesome-logo-clip-art-video-icon-5ac5bcbf5e7a50.846113781522908351387.jpg"><a href="#">&nbsp&nbspVideo: How long does it take for my campaign to be approved?</a></li>
+          <li>&nbsp&nbsp<img class="textDocIcon" src="http://www.anasfim.com/p/2018/03/text-document-icons-free-download-for-document-template-icon-600x600.jpg"><a href="#">&nbsp&nbspHow long does it take for my campaign to be approved?</a></li>
         </ul>
       </div>
     </div>
@@ -212,40 +218,15 @@
             </div>
         </div>
     </div>
-</section>
+  </section>
 </div>
+
+
 @endsection
-<!--   </body>
-</html> -->
-<!-- <script>
-  <a id="some_id" class="optional_has_click">Click Me</a>
 
-<script type="text/javascript">
-  $(document).ready(function(){
-
-    $("#some_id").click(function(){
-
-      // do something
-      alert( $(this).attr("id") );
-
-    });
-
-  });
-</script> -->
-
-<!-- var linkList = {
-  topic1 : [subtopic1, subtopic2, subtopic3],
-  topic2 : [subtopic1, subtopic2],
-  topic3 : [subtopic1, subtopic2, subtopic3, subtopic4]
-}
-
-$(linkList).each(function(i, item) {
-  $("<li/>".html($()))
-}) -->
 
 
 <!--
 @endsection
-@section('js')
 
-@endsection
+
